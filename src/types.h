a49b7ae5914563a54c210967072cdb96c3408ded
Joshua Murphy <hello@joshmurphy.ca>
2025-10-21 17:20:46 -0230

    Continued PCX parser refactor

--- src/types.h
+++ src/types.h
@@ -5,26 +5,16 @@
 #include <zip.h>
 #include <GLFW/glfw3.h>
 
-enum class PakFormat
-{
+enum class PakFormat {
     PAK,   // Original Quake/Quake 2 .pak format
     PKZIP, // ZIP-based formats (PK3, PK4, etc.)
     UNKNOWN
 };
 
-struct PakFileEntry
-{
+struct PakFileEntry {
     std::string filename;
     uint32_t offset;
     uint32_t size;
     PakFormat format;
     zip_file_t *zipFile;
-};
-
-struct PCXImage
-{
-    int width;
-    int height;
-    GLuint textureID;
-    std::string filename;
 };
